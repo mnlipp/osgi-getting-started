27bf68f5aaeebd57d9efc30fa3e338fba9bab75b
Michael N. Lipp <mnl@mnl.de>
2019-03-30 18:25:24 +0100

    Update continued.

---
+++ SimpleBundle-logging/src/io/github/mnl/osgiGettingStarted/loggingBundle/HelloWorld.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package io.github.mnl.osgiGettingStarted.loggingBundle;
+
+/**
+ * @author mnl
+ *
+ */
+public class HelloWorld extends Thread {
+
+    @Override
+    public void run() {
+        System.out.println("Hello World!");
+        while (!isInterrupted()) {
+            try {
+                // Activator.logService.log(LogService.LOG_INFO, "Hello Word sleeping");
+                sleep (5000);
+            } catch (InterruptedException e) {
+                break;
+            }
+        }
+    }
+    
+}
